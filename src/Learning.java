396f604f8805f49c23fdbb76f2e789f514fc8748
Elza Koylubaeva <zakirova_elza@mail.ru>
2022-07-19 11:20:03 +0500

    some fix

--- src/Learning.java
+++ src/Learning.java
@@ -2,4 +2,6 @@ public class Learning {
     private String test;
 
     private int num;
+
+    private double money;
 }
--- src/Study.java
+++ src/Study.java
@@ -1,3 +1,6 @@
 public class Study {
+
     private int study;
+
+    private int u;
 }
